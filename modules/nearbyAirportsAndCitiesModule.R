491e4c74fc01123d0a01ff1661a2d8b98b96d1cc
Mwangi George <mwangigeorge648@gmail.com>
2023-12-29 14:03:25 +0300

    secured API Key

--- modules/nearbyAirportsAndCitiesModule.R
+++ modules/nearbyAirportsAndCitiesModule.R
@@ -12,7 +12,7 @@ nearbyAirportsAndCitiesServer <- function(id, userLatitude, userLongitude, userD
   moduleServer(id, function(input, output, session) {
     df <- eventReactive(triggerId, {
       apiBase <- "https://airlabs.co/api/v9/nearby"
-      apiKey <- "8112ce34-bcbf-4246-9dac-8fa34dd14f3a"
+      apiKey <- Sys.getenv("airLabsApiKey")
 
       nearbyAPI <- str_c(
         apiBase, "?lat=", userLatitude, "&lng=", userLongitude, "&distance=", userDistance, "&api_key=", apiKey
